150fd6b0c9f1dfdf615db136b0458e3e5dbebeb1
Flutz17 <90649629+Flutz17@users.noreply.github.com>
2024-03-21 15:13:25 -0400

    Update vite.config.ts

--- WebIndecision/vite.config.ts
+++ WebIndecision/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '10.0.0.110' /*'51.222.107.87'*/
+  }
 })
